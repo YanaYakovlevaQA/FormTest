a1d571b36a46dce2ccb3f17bf2b985de6b7b64e1
Yana Yakovleva <yanna2013yanna@gmail.com>
2025-07-27 16:40:37 +0500

    add SelenideElement

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,11 +1,17 @@
 package pages.components;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
+    SelenideElement createMonth = $(".react-datepicker__month-select"),
+    createYear = $(".react-datepicker__year-select");
+
         public void setDate (String day, String month, String year) {
-            $(".react-datepicker__month-select").selectOption(month);
-            $(".react-datepicker__year-select").selectOption(year);
-            $(".react-datepicker__day--0" + day).click();
+            createMonth.selectOption(month);
+            createYear.selectOption(year);
+            $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
         }
 }
