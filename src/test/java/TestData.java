412a9ffecfd49c4ed1f023b74108eea1ada65b73
Yana Yakovleva <yanna2013yanna@gmail.com>
2025-07-27 16:42:29 +0500

    add SelenideElement

--- src/test/java/TestData.java
+++ src/test/java/TestData.java
@@ -9,22 +9,22 @@ public class TestData {
     public static Date DateOfBirthday = faker.date().birthday();
 
     public static String firstName = faker.name().firstName(),
-                         lastName = faker.name().lastName(),
-                         userEmail = faker.internet().emailAddress(),
-                         gender = faker.options().option("Male", "Female", "Other"),
-                         phoneNumber = faker.phoneNumber().subscriberNumber(10),
-                         day = String.valueOf(faker.random().nextInt(10, 27)),
-                         month = (new SimpleDateFormat("MMMM", Locale.ENGLISH).format(DateOfBirthday)),
-                         year = String.valueOf(faker.random().nextInt(1992, 2012)),
-                         subjects = faker.options().option( "Maths", "English", "Physics", "Chemistry",
-                                 "Computer Science", "Economics"),
-                         subjects1 = faker.options().option( "Social Studies", "History", "Civics",
+            lastName = faker.name().lastName(),
+            userEmail = faker.internet().emailAddress(),
+            gender = faker.options().option("Male", "Female", "Other"),
+            phoneNumber = faker.phoneNumber().subscriberNumber(10),
+            day = String.valueOf(faker.random().nextInt(10, 27)),
+            month = (new SimpleDateFormat("MMMM", Locale.ENGLISH).format(DateOfBirthday)),
+            year = String.valueOf(faker.random().nextInt(1992, 2012)),
+            subjects = faker.options().option("Maths", "English", "Physics", "Chemistry",
+                    "Computer Science", "Economics"),
+            subjects1 = faker.options().option("Social Studies", "History", "Civics",
                     "Commerce", "Biology"),
-                         hobbies = faker.options().option("Reading", "Sports", "Music"),
-                         picture = "PhotoToo.png",
-                         address = faker.address().fullAddress(),
-                         selectState = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan"),
-                         city = selectCity(selectState);
+            hobbies = faker.options().option("Reading", "Sports", "Music"),
+            picture = "PhotoToo.png",
+            address = faker.address().fullAddress(),
+            selectState = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan"),
+            city = selectCity(selectState);
 
 
     public static String selectCity(String state) {
@@ -37,4 +37,4 @@ public class TestData {
         };
 
     }
-    }
+}
